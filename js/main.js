ada6e459e34749c0baa28667afa6e63a9c00e7f6
Erik Ruhmanis <erik2310@edu.easj.dk>
2018-09-06 14:42:39 +0200

    Har lavet FizzBuzz.

--- js/main.js
+++ js/main.js
@@ -8,3 +8,32 @@ for (let i = 0; i < 7; i++) {
     triangle = triangle + "#";
 }
 
+
+console.log("");
+console.log("FizzBuzz:");
+console.log("");
+
+// Udskriver tal fra 1 til 100
+for (let i = 1; i <= 100; i++) {
+
+    // Hvis tallet kan divideres med 3 og ikke 5
+    if (i % 3 === 0 && i % 5 !== 0) {
+
+        console.log("Fizz");
+
+        // Hvis tallet kan divideres med 5 og ikke 3
+    } else if (i % 5 === 0 && i % 3 !== 0) {
+
+        console.log("Buzz");
+
+        // Hvis tallet kan divideres med 3 og 5
+    } else if (i % 3 === 0 && i % 5 === 0) {
+        
+        console.log("FizzBuzz");
+
+    } else {
+
+        console.log(i);
+
+    }
+}
